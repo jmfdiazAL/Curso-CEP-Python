4e5d41a3ec8f3fe23b80651db6326292fb1ceb6e
José Manuel Fernández Díaz <josemanuel.fernandez@iesalandalus.org>
2025-11-06 22:12:17 +0100

    Ejercicio Módulo 4

---
+++ M4/libro.py
@@ -0,0 +1,27 @@
+class Libro:
+    def __init__(self, titulo, autor, isbn):
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponible = True
+    
+    def prestar(self):
+        if self.disponible:
+            self.disponible = False
+            return True
+        return False
+
+    def devolver(self):
+        if not self.disponible:
+            self.disponible = True
+
+    def __str__(self):
+        return f'Título: {self.titulo}, Autor: {self.autor}, ISBN: {self.isbn}, Disponible: {self.disponible}'
+
+    def devolver(self):
+        if self._prestado:
+            self._prestado = False
+            self.disponible = True
+
+    def __str__(self):
+        return f'Título: {self.titulo}, Autor: {self.autor}, ISBN: {self.isbn}, Disponible: {self.disponible}'
